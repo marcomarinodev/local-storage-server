ba56f6a6b621b01ff2c6539d2fe87c22d9cee747
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2021-05-12 14:25:48 +0200

    Delete file-server-storage directory

--- file-server-storage/headers/arg_parser.h
+++
@@ -1,9 +0,0 @@
-#if !defined(ARG_PARSER_H_)
-#define ARG_PARSER_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "queue.h"
-
-#endif
--- file-server-storage/run_multiclient.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ((i=0;i<100;i+=1)); do
-    ./client
-done
-
